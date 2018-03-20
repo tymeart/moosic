a0caa1b6d329ddef57ec3f246d59d5076d01b58b
Tiffany Lam <tlam5@sbcglobal.net>
2018-03-20 16:01:49 -0700

    Initialize repo

---
+++ README.md
@@ -0,0 +1,1 @@
+A music player built in React.
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">a moosic player</h1>
+        </header>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
