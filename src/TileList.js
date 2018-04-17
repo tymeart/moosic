557e271b3c1172c55979ddb1bc955b221fa719fa
Tiffany Lam <tlam5@sbcglobal.net>
2018-04-16 17:24:37 -0700

    Create component for list of tiles

---
+++ src/TileList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const TileList = (props) => {
+  const listItems = props.arr.map(item => {
+    return (
+      <li className="tile">
+        <div className="thumbnail"></div>
+        <p>{item.title}</p>
+        {item.artst && <p>{item.artist}</p>}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+
+      <section>
+        <h2></h2>
+        <ul>
+          {listItems}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default TileList;
