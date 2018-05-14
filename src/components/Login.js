324a3f9cc3b242c7913ab1c8925436c7065d518d
Tiffany Lam <tlam5@sbcglobal.net>
2018-05-14 14:37:31 -0700

    Make login page background white

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import { CLIENT_ID, REDIRECT_URI } from '../hidden';
 class Login extends Component {
   render() {
     return (
-      <div className="center">
+      <div className="Login center">
         <h1>Welcome to Moosic</h1>
         <p>To continue, please log in to Spotify</p>
         <a href={`https://accounts.spotify.com/authorize/?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`}>
