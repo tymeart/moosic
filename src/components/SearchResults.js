57a20158b0c49be81d3bce4b375b6cacba4d3141
Tiffany Lam <tlam5@sbcglobal.net>
2018-06-04 14:32:11 -0700

    Render 12 album and playlist results

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -76,10 +76,63 @@ const SearchResults = ({ results }) => {
       </div>
 
       <h2>Albums</h2>
-      <div className="albums-results"></div>
+      <div className="albums-results">
+        <ul className="tilelist">
+          {results.albums.items.filter(album => {
+            return (
+              (album.images.length !== 0) && 
+              (album.images[0].height === album.images[0].width)
+            );
+          })
+            .slice(0, 12)
+            .map(album => {
+            return (
+              <li
+                className="tile"
+                key={album.id}
+              >
+                <img
+                  className="thumbnail"
+                  alt={`Thumbnail for album ${album.name}`}
+                  src={`${album.images[0].url}`}
+                />
+                <div className="albums-results__album">{album.name}</div>
+                <div className="albums-results__artist">{album.artists[0].name}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
       <h2>Playlists</h2>
-      <div className="playlists-results"></div>
+      <div className="playlists-results">
+        <ul className="tilelist">
+          {results.playlists.items.filter(playlist => {
+            return (
+              (playlist.images.length !== 0) &&
+              (playlist.images[0].height === playlist.images[0].width) && 
+              (playlist.owner.display_name !== null)
+            );
+          })
+            .slice(0, 12)
+            .map(playlist => {
+            return (
+              <li
+                className="tile"
+                key={playlist.id}
+              >
+                <img
+                  className="thumbnail"
+                  alt={`Thumbnail for playlist ${playlist.name}`}
+                  src={`${playlist.images[0].url}`}
+                />
+                <div className="playlists-results__playlist">{playlist.name}</div>
+                <div className="playlists-results__owner">{playlist.owner.display_name}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
