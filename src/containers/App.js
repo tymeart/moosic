7f6c4069fd0354879993264069f09910982724e0
Tiffany Lam <tlam5@sbcglobal.net>
2018-08-28 16:04:16 -0700

    Add env variables for router basename

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,7 @@ import {
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <div className="App-top">
             {this.props.isLoggedIn && <Navbar />}
