8c3f261468e6f92e69a2276a8f6163ce605ebd54
Tiffany Lam <tlam5@sbcglobal.net>
2018-11-26 17:34:02 -0800

    Updates

---
+++ precache-manifest.525284ad8227630e120fc6a9e776d9ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf35184ab8b3e0560bee",
+    "url": "/moosic/static/js/runtime~main.bf35184a.js"
+  },
+  {
+    "revision": "69d92e95be9a9c2b569f",
+    "url": "/moosic/static/js/main.69d92e95.chunk.js"
+  },
+  {
+    "revision": "9b2d4e11fcb0abbad2b4",
+    "url": "/moosic/static/js/1.9b2d4e11.chunk.js"
+  },
+  {
+    "revision": "69d92e95be9a9c2b569f",
+    "url": "/moosic/static/css/main.6e39d9d3.chunk.css"
+  },
+  {
+    "revision": "97ce51711d2902e375e3a7c87c5e390c",
+    "url": "/moosic/index.html"
+  }
+];
